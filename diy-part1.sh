02a673d3a5891812448f3bfef322691d5322f54b
mademlacloi <77881040+mademlacloi@users.noreply.github.com>
2021-09-18 18:02:27 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 Add a feed source
+echo "src-git packages https://github.com/immortalwrt/packages.git" > ./feeds.conf.default
+echo "src-git luci https://git.openwrt.org/project/luci.git" > ./feeds.conf.default
+echo "src-git routing https://git.openwrt.org/feed/routing.git" > ./feeds.conf.default
+echo "src-git telephony https://git.openwrt.org/feed/telephony.git" > ./feeds.conf.default
+#
 echo "src-git Goldenorb https://github.com/ROOterDairyman/Goldenorb.git " >> ./feeds.conf.default
 echo "src-git luci_proto_modemmanager https://github.com/nickberry17/luci-proto-modemmanager.git" >> ./feeds.conf.default
 echo "src-git modeminfo https://github.com/koshev-msk/luci-app-modeminfo.git" >> ./feeds.conf.default
