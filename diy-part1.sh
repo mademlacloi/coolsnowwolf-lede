f38c0df3f955194519ebcc8b7ef63b0750089f8e
mademlacloi <77881040+mademlacloi@users.noreply.github.com>
2021-09-18 13:10:35 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,12 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+Add a feed source
+echo "src-git Goldenorb https://github.com/ROOterDairyman/Goldenorb.git " >> ./feeds.conf.default
+echo "src-git luci_proto_modemmanager https://github.com/nickberry17/luci-proto-modemmanager.git" >> ./feeds.conf.default
+echo "src-git immortalwrt https://github.com/immortalwrt/packages.git" >> ./feeds.conf.default
+echo "src-git modeminfo https://github.com/koshev-msk/luci-app-modeminfo.git" >> ./feeds.conf.default
+echo "src/gz 123ru_luci http://openwrt.132lan.ru/packages/packages-19.07/luci" >> ./feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
