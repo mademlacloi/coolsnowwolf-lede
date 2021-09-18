658764251755ea3048486d256ccfe1baf03543c2
mademlacloi <77881040+mademlacloi@users.noreply.github.com>
2021-09-18 18:39:32 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,6 @@ echo "src-git packages https://github.com/immortalwrt/packages.git" > ./feeds.co
 echo "src-git luci https://git.openwrt.org/project/luci.git" >> ./feeds.conf.default
 echo "src-git routing https://git.openwrt.org/feed/routing.git" >> ./feeds.conf.default
 echo "src-git telephony https://git.openwrt.org/feed/telephony.git" >> ./feeds.conf.default
-#
-echo "src-git Goldenorb https://github.com/ROOterDairyman/Goldenorb.git " >> ./feeds.conf.default
-echo "src-git luci_proto_modemmanager https://github.com/nickberry17/luci-proto-modemmanager.git" >> ./feeds.conf.default
-echo "src-git modeminfo https://github.com/koshev-msk/luci-app-modeminfo.git" >> ./feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
