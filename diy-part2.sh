cc3c242be26af4cd49f2e3feca524917e496d1f6
mademlacloi <77881040+mademlacloi@users.noreply.github.com>
2021-10-09 01:29:06 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@
 
 #    - name: add mbim feeds
 #      run: cd openwrt/package/lean && 
-git clone https://github.com/ROOterDairyman/Goldenorb/tree/master/luci-proto-mbim
+#git clone https://github.com/ROOterDairyman/Goldenorb/tree/master/luci-proto-mbim
 
 #    - name: add accesscontrol feeds
 #      run: cd openwrt/package/lean &&
